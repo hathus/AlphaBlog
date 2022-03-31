133f2ba3b4a498a7e646b921671701d6079bc470
Hathus <trilogy@gmail.com>
2022-03-31 09:29:56 -0600

    Change a format and add container to new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = User.last
 
     respond_to do |format|
       if @article.save
