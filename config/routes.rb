eefd0bbe0e8003b325979f5c51de72ca5d74759a
Hathus <trilogy@gmail.com>
2022-04-14 12:38:49 -0500

    Add categories

--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,5 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   # get 'logout', to: 'sessions#destroy'
   delete 'logout', to: 'sessions#destroy'
-  #resources :categories, except: [:destroy]
+  resources :categories, except: [:destroy]
 end
