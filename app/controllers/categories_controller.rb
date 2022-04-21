1d8e7b0585cee8fec23bb827fa23881557a29d4f
Hathus <trilogyti@gmail.com>
2022-04-21 18:13:39 -0500

    Add categories functions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,9 @@
 class CategoriesController < ApplicationController
 
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
+
   def index
+    @categories = Category.all
   end
 
   def new
@@ -18,9 +21,34 @@ class CategoriesController < ApplicationController
   end
 
   def show
+    # @category = Category.find(params[:id])
+  end
+
+  def edit 
+    # @category = Category.find(params[:id])
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:notice] = "Category was successfully updated"
+      redirect_to category_url
+    else
+      flash[:alert] = "An erros was found, unprocessable entity"
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @category.destroy
+    flash[:notice] = "The category was successfully deleted"
+    redirect_to categories_url
   end
 
   private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
   def category_params
     params.require(:category).permit(:name)
